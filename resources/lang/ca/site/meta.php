2b48ca596b44bc21dacc0da6aaf58738578ce221
Jaume MK <jaumemk@gmail.com>
2019-09-02 20:03:06 +0800

    adding language line support

--- resources/lang/ca/site/meta.php
+++ resources/lang/ca/site/meta.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'untitled-page' => 'Pàgina sense títol | ', // comenting or removing is safe
+    'untitled-page' => 'Pàgina sense títol | ',
     'title' => 'Yasha Base',
-    'description' => 'Yasha Base is a basic, extensible and fully customizable template for a quick set up of frontend & backend developments.'
+    'description' => 'Descripció de la página',
+    'keywords' => 'Paraules clau'
 ];
